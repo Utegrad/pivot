a5f089a573726115fa8a90a998e0d9a60bc97d76
Matthew Larsen <utegrad@gmail.com>
2013-11-15 17:02:34 -0800

    .

--- nflPlayerScoreSummary.php
+++ nflPlayerScoreSummary.php
@@ -10,7 +10,7 @@ require_once APP_ROOT . 'Classes/FfPosition.php';
 require_once APP_ROOT . 'Classes/FfTeam.php';
 require_once APP_ROOT . 'Classes/FfLeague.php';
 
-$token = Api::TouchToken();
+$token = Api::TouchToken('U2FsdGVkX1_laPUP2JFms7EWi43KMWANsx3nNMPdlvv3-zQZNRHCprnNXNz3UR2PSw3oSxV675pnTdWl_SzXGWEwjSb928i5RAHs4uvT2VwITmyNiYt6VQLXk3Rnd8Ks');
 if ($token == FALSE) {
 	echo "<h2>Problem getting token from the database or from _GET</h2>";
 	echo "<p><a href='". $APP_URL ."/View/PopulateDatabase/index.php'>Index</a></p>";
@@ -33,6 +33,7 @@ $nflPlayerWeeklyScoring = CBSSports::GetJsonData(CBSSports::FPSWS, 'player_statu
 
 $ffTeamRosters = CBSSports::GetJsonData(CBSSports::ROSTERS, 'all');
 
+$playerSummarys = array();
 $nflPlayerSets = array();
 foreach($nflPlayerData['data']->body->players as $player){
 	$p = NFLPlayer::withJson($player);
@@ -59,20 +60,33 @@ foreach($nflPlayerData['data']->body->players as $player){
 			}
 		}
 	}
+	$playerSummary = array(
+		'id' => $p->Id,
+		'fullName' => $p->FullName,
+		'pos' => $p->Pos,
+		'nflTeam' => $p->NFLTeam,
+		'bye' => $p->ByeWeek,
+		'fTeam' => $p->FfTeam,
+		'avg' => 'Need to calc this',
+		'sd' => 'Need to calc this',
+		'Rating' => 'Need to calc this',
+	);
 	
 	$set = array('player' => $p, 'periods' => $periods, );
 	array_push($nflPlayerSets, $set);
 }
 
 
+
 foreach($nflPlayerSets as $set){
 	if(!(empty($set['player']->FfTeam->Name))){
-		echo "<p>Player : ". $set['player']->FullName ." (". $set['player']->Pos .") with ID : ". $set['player']->Id ." on team : ". $set['player']->FfTeam->Name ."</p>";
-		echo "<p>Scores:<br>";
+		$id = $set['player'];
+		$fullName;
+		
 		foreach ($set['periods'] as $period){
-			echo "Period ". $period->period ." Score: ". $period->score ."<br>";
+			
 		}
-		echo "</p>";
+		
 	}
 }
 //require 'page.php';
